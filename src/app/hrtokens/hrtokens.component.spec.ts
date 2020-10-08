04d53125f08d38c2eff28d05785c82d13b46b630
ramyaa12345 <aramya11282@gmail.com>
2020-10-08 11:34:35 +0530

    query and session services

---
+++ src/app/hrtokens/hrtokens.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HrtokensComponent } from './hrtokens.component';
+
+describe('HrtokensComponent', () => {
+  let component: HrtokensComponent;
+  let fixture: ComponentFixture<HrtokensComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HrtokensComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HrtokensComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
